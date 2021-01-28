e621cf3a768ac6628420a5513028f6fe3e7acd67
adawd007 <59585908+adawd007@users.noreply.github.com>
2021-01-28 10:06:33 -0600

    adding files

---
+++ FeastFreedom/src/test/java/com/example/feastfreedom/entity/FeastFreedomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.feastfreedom.entity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeastFreedomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
