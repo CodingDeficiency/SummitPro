06fa90cdfc0576911e1543b75a41c00b8b97966c
adawd007 <59585908+adawd007@users.noreply.github.com>
2021-02-01 08:44:15 -0600

    Revert "Revert "beans""
    This reverts commit ee134148e4f94cc092fe640cd625d34b32f2d450.

--- FeastFreedom/src/main/java/com/example/feastfreedom/entity/Order.java
+++ FeastFreedom/src/main/java/com/example/feastfreedom/entity/Order.java
@@ -1,5 +1,21 @@
 package com.example.feastfreedom.entity;
 
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Order123")
 public class Order {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	private int id;
+	private Date timeOrd;
+	private float amountPaid;
+	
 
 }
